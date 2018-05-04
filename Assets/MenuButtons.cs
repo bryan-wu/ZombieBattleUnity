f4cd55717acabc1f8277eaf6b9550b31006791dc
meredithxu <yiyangxu@umail.ucsb.edu>
2018-05-03 21:01:48 -0700

    added badic menu with play button working

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuButtons : MonoBehaviour {
+
+public void SwitchPage (string pageName)
+    {
+        SceneManager.LoadScene(pageName, LoadSceneMode.Single);
+    }
+}
