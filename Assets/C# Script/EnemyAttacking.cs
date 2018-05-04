85011f8712b2d56c7eca7eccf5cc28cf6769aa03
Bryan Wu <bryanwu@umail.ucsb.edu>
2018-05-03 17:26:25 -0700

    finished zombies

---
+++ Assets/C# Script/EnemyAttacking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttacking : MonoBehaviour {
+    // Last Attack Time
+    float last = 0;
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        // Collided with a Plant?
+        if (coll.gameObject.tag == "Ally")
+        {
+            // Play Attack Animation
+            GetComponent<Animator>().SetTrigger("IsAttacking");
+            // Deal damage once a second
+            if (Time.time - last >= 1)
+            {
+                coll.gameObject.GetComponent<Health>().doDamage(1);
+                last = Time.time;
+            }
+        }
+    }
+}
