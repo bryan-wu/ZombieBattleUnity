96be2fa069a8bcb4f6d1cf03fda377de670423c6
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-18 12:18:31 -0700

    BulletDamage commented

--- Assets/C# Script/BulletDamage.cs
+++ Assets/C# Script/BulletDamage.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BulletDamage : MonoBehaviour {
 
+    //Pre-conditon: a bullet is fired and flying
+
+    //Post-condition: collide with the object if it is a zombie
+    //damage zombie's health by 1
+    //destory the bullet
     void OnTriggerEnter2D(Collider2D co)
     {
         // check if it's a zombie
