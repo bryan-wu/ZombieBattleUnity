092f1023083f99754880b6021a01f0cf62bbbc66
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-18 14:00:20 -0700

    MoneySpawn commented

--- Assets/C# Script/MoneySpawn.cs
+++ Assets/C# Script/MoneySpawn.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoneySpawn : MonoBehaviour {
-    public GameObject prefab;
+
+    //Prefab of Sun
+    public GameObject SunPrefab;
+
     // Use this for initialization
+    //Post-condition: Repeat spawning every time interval
     void Start()
     {
         // Spawn first Money in 10 seconds, repeat every 10 seconds
         InvokeRepeating("Spawn", 10, 10);
     }
 
+    //Post-condition: generate a sun prefab
     void Spawn()
     {
         // Load prefab into the Scene
-        Instantiate(prefab,
+        Instantiate(SunPrefab,
                     transform.position,
                     Quaternion.identity);
     }
