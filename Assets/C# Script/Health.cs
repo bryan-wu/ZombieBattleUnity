26e0e520d5fa00c9f7aa202e387ae843c3fa28ed
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-18 13:33:27 -0700

    Health commented

--- Assets/C# Script/Health.cs
+++ Assets/C# Script/Health.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
+    
     // Current Health
     [SerializeField]
     int cur = 5;
 
+    //Post-condition: object's health get damaged
+    // object destoried if health goes down to zero
     public void doDamage(int n)
     {
         // Subtract damage from current health
