9e70eb11dcb93e6a180474fe69bd240e7b3b5f83
meredithxu <yiyangxu@umail.ucsb.edu>
2018-05-17 22:37:05 -0700

    fixed bug that you can place multiple soldiers at the same position

--- Assets/C# Script/Grass.cs
+++ Assets/C# Script/Grass.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Grass : MonoBehaviour {
+    //post-condition: return false if there's already a soldier at position newPos
+    //Otherwise return true
+    bool CheckIfEmpty(Vector3 newPos)
+    {
+        GameObject[] zom = GameObject.FindGameObjectsWithTag("Ally");
+        foreach(GameObject obj in zom)
+        {
+            if(obj.transform.position == newPos)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    //post-condition: selected soldier is placed at the grid clicked
     void OnMouseUpAsButton()
     {
-        // Is there something to build?
         if (BuildMenu.cur != null)
         {
-            // Build it
-            Instantiate(BuildMenu.cur.gameObject, transform.position, Quaternion.identity);
-            MoneyCollect.score -= BuildMenu.cur.price;
-            BuildMenu.cur = null;
+            Vector3 targetPos = transform.position;
+            if (CheckIfEmpty(targetPos))
+            {
+                Instantiate(BuildMenu.cur.gameObject, targetPos, Quaternion.identity);
+                MoneyCollect.score -= BuildMenu.cur.price;
+                BuildMenu.cur = null;
+            }
         }
     }
 }
