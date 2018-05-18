db92ca5ecbb76e6d8ff2ac76dddde0c959e910c1
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-18 12:11:10 -0700

    BuildInfo commented

--- Assets/C# Script/BuildInfo.cs
+++ Assets/C# Script/BuildInfo.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildInfo : MonoBehaviour {
+
+    //The image of item shown of the shop
     public Texture previewImage;
+
+    //item's price
     public int price;
 }
