5cf1f7606230861c1616b32a0bb7da9704e7e099
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-25 11:05:40 -0700

    music seamlessly play

---
+++ Assets/C# Script/MusicPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlay : MonoBehaviour {
+
+  //pre-condition: an music gameobject is ready and attached to scene
+  //post-condition: background enter music is played seamlessly
+   void Awake()
+    {
+        GameObject[] Musics = GameObject.FindGameObjectsWithTag("Music");
+
+        if (Musics.Length > 1)
+            Destroy(this.gameObject);            //check if there is more than one music playing
+                                                 //if so, delete the current one so that keep the previous playing one
+
+        DontDestroyOnLoad(this.gameObject);       
+    }
+}
