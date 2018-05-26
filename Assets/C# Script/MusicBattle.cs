62742b0ad7f00fb70c060728d5feafdf42f46c78
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-25 22:51:11 -0700

    2 background music complete

---
+++ Assets/C# Script/MusicBattle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBattle : MonoBehaviour {
+
+    void Awake()
+    {
+        GameObject[] Musics = GameObject.FindGameObjectsWithTag("Music");
+
+        foreach(GameObject mus in Musics)
+            Destroy(mus);
+
+    }
+}
