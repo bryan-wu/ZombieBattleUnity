b79750d640db18d94e33f0097b9e6890b9d7ed09
DennisZZH <zihaozhang@umail.ucsb.edu>
2018-05-28 19:41:24 -0700

    raw sound effect

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
