1ccae5c002a48f644e3d6de4130b0a3f9ee1bd22
nikkityagi <nikitatyagi@umail.ucsb.edu>
2018-06-02 17:03:49 -0700

    fixed reset game shop bug and added win condition

--- Assets/C# Script/GameController.cs
+++ Assets/C# Script/GameController.cs
@@ -7,14 +7,14 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public Text waveMes,loseMes;
+    public Text waveMes,loseMes, winMes;
     public GameObject Zombies;
     public GameObject Zombieboss;
     public GameObject CrazyZombies;
     public GameObject HotDog;
     public float SpawnValuesX, TimeBetweenSpawns, TimeBetweenWaves, ReducedTimeBetWaves;
     public int ZombiesCount, IncreaseZombies, SpawnValuesYLower, SpawnValuesYUpper;
-    public static bool lose;
+    public static bool lose, win;
     //pos-condition: return false if they spawn at the same position
     //otherwise return true
     bool CheckBeforeSpawn(Vector3 newPos)
@@ -34,6 +34,8 @@ public class GameController : MonoBehaviour
     {
         loseMes.text = "";
         lose = false;
+        winMes.text = "";
+        win = false;
         StartCoroutine(SpawnZombies());
     }
 
@@ -44,6 +46,27 @@ public class GameController : MonoBehaviour
             loseMes.text = "You lost the game! Press 'N' to restart the game";
             if (Input.GetKey(KeyCode.N))
             {
+                MoneyCollect.score = 500;
+                SceneManager.LoadScene("project");
+            }
+        }
+
+        
+        if (TimeBetweenWaves == 0)
+        {
+            GameObject[] zom = GameObject.FindGameObjectsWithTag("Zombie");
+            if (zom.Length == 0)
+            {
+                win = true;
+            }
+        }
+
+        if (win)
+        {
+            winMes.text = "You've won the game! Press 'N' to restart the game";
+            if (Input.GetKey(KeyCode.N))
+            {
+                MoneyCollect.score = 500;
                 SceneManager.LoadScene("project");
             }
         }
